0469375b6d08bf86c2d1c02b4738d460bca2f476
popeopzy <popeopzy@gmail.com>
2024-04-29 14:16:47 +0100

    task-6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,13 @@
 from flask import Blueprint
-from api.v1.views.index import *
 
-app_view = Blueprint('app_view', __name__, url_prefix='api/v1')
+app_views = Blueprint('app_view', __name__, url_prefix='api/v1')
+
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    # from api.v1.views.cities import *
+    # from api.v1.views.amenities import *
+    # from api.v1.views.users import *
+    # from api.v1.views.places import *
+    # from api.v1.views.places_reviews import *
+    # from api.v1.views.places_amenities import *
--- api/v1/views/app_views.py
+++ api/v1/views/app_views.py
@@ -1,4 +1,39 @@
-from flask import Blueprint
+#!/usr/bin/python3
 
-app_views = Blueprint("app_views", __name__)
+from flask import Flask, make_response, jsonify
+from models import storage 
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/app/*": {"origins": "0.0.0.0"}})
+
+app.url.map.strict_slashes = False
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tear(self):
+    """closes the storage"""
+    storage.close()
+    
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 error"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+
+
+
+
 
