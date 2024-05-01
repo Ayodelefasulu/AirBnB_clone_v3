2095ce03584ffe4aad3835c87c9a29863ac79bd9
Ayodelefasulu <fasuluayodele@gmail.com>
2024-05-01 05:46:34 -0700

    task 3 in app.py imported os

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
 """ Index page of the app """
-from api.v1.views import app_views
+
 from flask import jsonify
 from models import storage
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-# Route /status
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """Endpoint to check the status of the API.
+
+    Returns:
+        JSON response containing the status.
+    """
     return jsonify({"status": "OK"})
 
 
-# Route /stats
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
-    """Retrieve the number of each object type"""
+    """Retrieve the number of each object type.
+
+    Returns:
+        JSON response containing the counts of different object types.
+    """
     stats = {
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
